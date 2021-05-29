4e13341cffc0173896b07e798356dceca314283b
Akshay Dwivedi <akshaydwivedi@live.com>
2021-05-29 10:19:06 +0530

    remove browser animation module
    remove browser animation module dependency (should only be imported in the main project)

--- projects/ng-orgchart/src/lib/components/orgchart/orgchart.module.ts
+++ projects/ng-orgchart/src/lib/components/orgchart/orgchart.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChartContainerComponent } from './chart-container/chart-container.component';
 import { ChartNodeComponent } from './chart-node/chart-node.component';
 import { NodeSelectService } from './shared/services/node-select.service';
@@ -9,7 +8,6 @@ import { NodeSelectService } from './shared/services/node-select.service';
   declarations: [ChartContainerComponent, ChartNodeComponent],
   imports: [
     CommonModule,
-    BrowserAnimationsModule
   ],
   exports: [ChartContainerComponent, ChartNodeComponent],
   providers: [NodeSelectService],
